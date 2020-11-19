a72d7090d8dccf487990f321b2cf07602f4e16df
lannguyen131198 <lannguyen131198@gmail.com>
2020-11-19 09:05:52 +0700

    second commit

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+using SportsStore.Models.ViewModels;
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List(string Category, int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                .Where(P => Category == null || P.Category == Category)
+                 .OrderBy(p => p.ProductID)
+                 .Skip((productPage - 1) * PageSize)//danh sách product
+                 .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,//thông tin phân trang
+                    TotalItems = Category == null ?
+                     repository.Products.Count() :
+                    repository.Products.Where(e =>
+                     e.Category == Category).Count()
+                },
+                CurrentCategory = Category
+            });
+    }
+}
---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+            Database.EnsureCreated();
+         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
